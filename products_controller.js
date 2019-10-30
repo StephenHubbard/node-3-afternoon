31d62303582d9071cab98ddfd1f534af011e41b2
Stephen <hubbard_stephen@yaho.com>
2019-10-30 14:46:55 -0600

    done pre black diamond tinkering

---
+++ products_controller.js
@@ -0,0 +1,59 @@
+module.exports = {
+    create: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+        const { name, description, price, image_url } = req.body;
+
+        dbInstance.create_product([name, description, price, image_url])
+            .then( () => res.sendStatus(200) )
+            .catch( err => {
+                res.status(500).send({errorMessage: "Whoops!  Something went wrong with our create_product fn!"})
+                console.log(err)
+            })
+    }, 
+    getOne: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+        const { id } = req.params;
+
+        dbInstance.read_product(id)
+        .then( product => res.status(200).send( product ) )
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oops! Something went wrong with our read_product fn!"});
+            console.log(err)
+        } );
+    },
+    
+    getAll: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+    
+        dbInstance.read_products()
+        .then( products => res.status(200).send( products ) )
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oops! Something went wrong with our read_product fn!"});
+            console.log(err)
+        } );
+    },
+    
+    update: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+        const { params, query } = req;
+    
+        dbInstance.update_product([params.id, query.desc])
+        .then( () => res.sendStatus(200) )
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oops! Something went wrong with our update_product fn!"});
+            console.log(err)
+        } );
+    },
+    
+    delete: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+        const { id } = req.params;
+    
+        dbInstance.delete_product(id)
+        .then( () => res.sendStatus(200) )
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oops! Something went wrong with our delete_product fn!"});
+            console.log(err)
+        } );
+    }
+}
